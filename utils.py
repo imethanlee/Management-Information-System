90cb3eb53733ebb6f9d81786e6f07b28d83e7edd
Leopold233 <meetleopold@hotmail.com>
2019-06-21 21:58:22 +0800

    window in center

--- utils.py
+++ utils.py
@@ -15,8 +15,8 @@ def center_window(root, width, height):
     screenwidth = root.winfo_screenwidth()
     screenheight = root.winfo_screenheight()
     size = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-    # print(size)
     root.geometry(size)
+    root.update()
 
 
 def clear_frame(root):
